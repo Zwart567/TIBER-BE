cf03d69e4c089c6cbe04a2b10c71e5d8390ec727
Zwart567 <nasrul.k.ikhwan@gmail.com>
2026-02-02 23:13:26 +0700

    Added Auth register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
+use App\Models\Personalization;
+
 class AuthController extends Controller
 {
     /**
@@ -48,5 +50,42 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Successfully logged out'
         ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'user.fullname'=>'required|string',
+            'user.email'=>'required|email|unique:users,email',
+            'user.password'=>'required',
+
+            'personalization.start_date'=>'required|date',
+            'personalization.duration_month'=>'required|integer',
+            'personalization.control_freq_value'=>'required|integer',
+            'personalization.control_freq_unit'=>'required|string',
+            'personalization.reminder_time'=>'required',
+            'personalization.time_category'=>'required|string',
+        ]);
+
+        $user = User::create([
+            'fullname'=> $request->input('user.fullname'),
+            'email'=> $request->input('user.email'),
+            'password'=>Hash::make($request->input('user.password')),
+        ]);
+
+        Personalization::create([
+            'user_id'=> $user->id,
+            'start_date'=> $request->input('personalization.start_date'),
+            'duration_month'=> $request->input('personalization.duration_month'),
+            'control_freq_value'=> $request->input('personalization.control_freq_value'),
+            'control_freq_unit'=> $request->input('personalization.control_freq_unit'),
+            'reminder_time'=> $request->input('personalization.reminder_time'),
+            'time_category'=> $request->input('personalization.time_category'),
+        ]);
+
+        return response()->json([
+            'status'=>'success',
+            'message'=>'User and personalization created. Please login to continue'
+        ],201);
     }
 }
